212d85db9a16c618dce85a3cdb317a7271df2115
Farhan Ali Raza <62690310+FarhanAliRaza@users.noreply.github.com>
2020-11-23 08:22:29 -0800

    Update settings.py

--- ecom/settings.py
+++ ecom/settings.py
@@ -10,7 +10,7 @@ PWA_SERVICE_WORKER_PATH = os.path.join(BASE_DIR, '/static/js', 'serviceworker.js
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3golj&eh91ui5$ojm1(3dnnhrql!efp377yz!47^wqg%gghts#'
+SECRET_KEY = 'Secret'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
